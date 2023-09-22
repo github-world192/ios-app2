781c76aef9db117c0be57c0cac199ab5162a76fa
github-world192 <dillen99999999@gmail.com>
2023-09-22 00:50:05 -0700

    Initial Commit

---
+++ ios-app2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ios-app2
+//
+//  Created by llll on 2023/9/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ios-app2/ios_app2App.swift
@@ -0,0 +1,17 @@
+//
+//  ios_app2App.swift
+//  ios-app2
+//
+//  Created by llll on 2023/9/22.
+//
+
+import SwiftUI
+
+@main
+struct ios_app2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
